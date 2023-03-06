10f8885c0fcf4154936da13dba45943a760e08a8
Vitalii Petrun <vitalii.petrun@gmail.com>
2023-03-06 12:27:27 +0200

    fix CI

--- lib/presentation/pages/admin_profile/transactions_page.dart
+++ lib/presentation/pages/admin_profile/transactions_page.dart
@@ -5,7 +5,6 @@ import 'package:meta_app/data/models/pending_transactions.dart';
 import 'package:meta_app/data/models/referrals.dart';
 import 'package:meta_app/data/models/transaction_history.dart';
 import 'package:meta_app/presentation/widgets/rights_reserved_footer.dart';
-import 'package:meta_app/presentation/widgets/transaction_table/transaction_table.dart';
 import '../../../data/models/transaction.dart';
 import '../../../data/models/user.dart';
 import '../../widgets/profile_header/profile_header.dart';
